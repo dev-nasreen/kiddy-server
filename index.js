d40fca7ffd5a78816805d3b0bed8a38a4cca1780
dev-nasreen <nasreen.akter34@gmail.com>
2021-04-18 23:52:10 +0600

    final change

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 require('dotenv').config()
-const port =process.env.PORT || 5000
+const port = process.env.PORT || 5000
 const MongoClient = require('mongodb').MongoClient;
-const  ObjectId  = require('mongodb').ObjectID;
+const ObjectId = require('mongodb').ObjectID;
 const { connect, ObjectID } = require('mongodb')
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ihq8y.mongodb.net/${process.env.DB_PASS}?retryWrites=true&w=majority`;
 
@@ -12,7 +12,7 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 
 
@@ -20,84 +20,136 @@ client.connect(err => {
   const serviceCollection = client.db("Kiddy").collection("allServices");
   const orderCollection = client.db("Kiddy").collection("allOrders");
   const reviewCollection = client.db("Kiddy").collection("allReview");
-  
-  app.post('/addService', (req, res) =>{
-      const newService = req.body;
-      serviceCollection.insertOne(newService)
-      .then(result =>{
-         // console.log(result.insertedCount)
-          res.send(result.insertedCount > 0)
+  const adminCollection = client.db("Kiddy").collection("allAdmin");
+
+  app.post('/addService', (req, res) => {
+    const newService = req.body;
+    serviceCollection.insertOne(newService)
+      .then(result => {
+        // console.log(result.insertedCount)
+        res.send(result.insertedCount > 0)
       })
   });
 
   // add review
-  app.post('/addReview', (req, res) =>{
-      const newReview = req.body;
-      reviewCollection.insertOne(newReview)
-      .then(result =>{
-         // console.log(result.insertedCount)
-          res.send(result.insertedCount > 0)
+  app.post('/addReview', (req, res) => {
+    const newReview = req.body;
+    reviewCollection.insertOne(newReview)
+      .then(result => {
+        // console.log(result.insertedCount)
+        res.send(result.insertedCount > 0)
       })
   });
 
- //call reviews on UI
- app.get('/reviews', (req, res) =>{
-  reviewCollection.find()
-  .toArray((err, document) =>{
-    res.send(document)
+  // add admin
+  app.post('/addAdmin', (req, res) => {
+    const newAdmin = req.body;
+    adminCollection.insertOne(newAdmin)
+      .then(result => {
+        // console.log(result.insertedCount)
+        res.send(result.insertedCount > 0)
+      })
+  });
+
+  //call admin on UI
+  app.get('/adminList', (req, res) => {
+    adminCollection.find()
+      .toArray((err, document) => {
+        res.send(document)
+      })
   })
-})
+  //call reviews on UI
+  app.get('/reviews', (req, res) => {
+    reviewCollection.find()
+      .toArray((err, document) => {
+        res.send(document)
+      })
+  })
 
 
- //call services on UI 
- app.get('/packages', (req, res) =>{
-  serviceCollection.find()
-  .toArray((err, document) =>{
-    res.send(document)
+  //call services on UI 
+  app.get('/packages', (req, res) => {
+    serviceCollection.find()
+      .toArray((err, document) => {
+        res.send(document)
+      })
   })
-})
 
-//book service order with payment details
-app.post('/addOrder', (req, res) =>{
-  orderCollection.insertOne(req.body)
-  .then(result =>{
-    res.send(result.insertedCount > 0);
-    //console.log(result.insertedCount)
+  //book service order with payment details
+  app.post('/addOrder', (req, res) => {
+    orderCollection.insertOne(req.body)
+      .then(result => {
+        res.send(result.insertedCount > 0);
+        //console.log(result.insertedCount)
+      })
   })
-})
 
-app.get('/singleService/:id', (req, res) =>{
-  serviceCollection.find({_id: ObjectID(req.params.id)})
-  .toArray((err, service) =>{
-    res.send(service)
+  app.get('/singleService/:id', (req, res) => {
+    serviceCollection.find({ _id: ObjectID(req.params.id) })
+      .toArray((err, service) => {
+        res.send(service)
+      })
   })
-})
 
- //call orders on UI
- app.get('/bookingList/:email', (req, res) =>{
-  console.log(req.params.email);
-  orderCollection.find({email: req.params.email})
-  .toArray((err, documents) =>{
-    res.send(documents)
+  
+ app.get('/orders', (req, res) =>{
+    console.log(req.query.email);
+    orderCollection.find({email: req.query.email})
+    .toArray((err, documents) =>{
+      res.send(documents)
+      console.log(documents)
+    })
   })
+
+  // Update order status
+  // app.patch('/updateStatus/:id', (req, res) => {
+  //  console.log(req.params.id);
+  //   orderCollection.updateOne({ _id: ObjectID(req.params.id) },
+  //     {
+  //       $set: { status: req.body.status }
+  //     })
+  //     .then(result => {
+  //       res.send(result.modifiedCount > 0);
+  //       console.log(result.modifiedCount);
+  //     })
+  // })
+  app.patch('/updateStatus', (req, res) => {
+    const order = req.body;
+    orderCollection.updateOne({ _id: ObjectID(req.body.id) },
+      { $set: {"status": req.body.status} }
+    )
+    .then(result => console.log(result))
+  })
+
+
+  
+   // Is Admin 
+   app.post('/isAdmin', (req, res) => {
+    const email = req.body.email;
+
+    adminCollection.find({ email: email })
+        .toArray((err, result) => {
+            res.send(result.length > 0)
+        })
 })
+  
 
   //call products on UI 
-  app.get('/availableServices', (req, res) =>{
+  app.get('/availableServices', (req, res) => {
     serviceCollection.find()
-    .toArray((err, services) =>{
-      res.send(services)
-    })
+      .toArray((err, services) => {
+        res.send(services)
+      })
   })
 
-  app.delete('/delete/:id',(req, res)=>{
-    const id =req.params.id;
-    serviceCollection.deleteOne({_id: ObjectId(id)})
-    .then(result =>{
-      res.send(result.deletedCount > 0)
-      
-    })
-  
+  app.delete('/delete/:id', (req, res) => {
+    const id = req.params.id;
+    serviceCollection.deleteOne({ _id: ObjectId(id) })
+      .then(result => {
+        res.send(result.deletedCount > 0)
+
+      })
+
   })
 
 
